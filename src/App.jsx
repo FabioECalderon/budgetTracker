036ac7509cc2bd947dc2a9a49dc81f6d0064ba6e
Fabio Calderon <FabiooCalderon@gmail.com>
2023-12-23 23:23:18 -0500

    SignUp and logIn pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { Col, Container, Row } from 'react-bootstrap';
+
+import { UserProvider } from './containers/UserContext';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <UserProvider>
+      <Container>
+        <Row>
+          <Col>Col1</Col>
+          <Col>Col2</Col>
+        </Row>
+      </Container>
+    </UserProvider>
+  );
 }
